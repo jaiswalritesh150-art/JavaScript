c8cb3b65c2e09d72dda7a38644805ddb6ff38638
Ritesh Jaiswal <jaiswalritesh150@gmail.com>
2026-05-26 11:58:20 +0000

    container config done

---
+++ 01.basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 1234754;
+let accountEmail = "rishu124@gmail.com";
+var accountPassword  = "6789";
+accountCity = "varanasi";
+let accountState ;
+
+// accountID = 2; // not allowed 
+
+accountEmail = "hafr@234.com";
+accountPassword = "56789";
+accountCity = "ghaz";
+
+console.log(accountID);
+
+console.table([accountID, accountEmail ,accountPassword, accountCity,accountState])
