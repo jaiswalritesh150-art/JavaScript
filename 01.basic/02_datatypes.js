c838c3ddd08a1a86da075d014fa33fc30402d378
Ritesh Jaiswal <jaiswalritesh150@gmail.com>
2026-05-27 08:52:08 +0000

    container config done

---
+++ 01.basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log("ritesh");
+
+let name = "ritesh";
+let age = 19;
+let isLoggedIn = false;
+
+//primitive datatype
+
+// number => 2 tp power 53
+// bigint
+// string ""
+// boolean => true/false;
+// null => standalone 
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null); 
+
