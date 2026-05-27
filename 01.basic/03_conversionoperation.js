8586bff7b8bf8250cf2d27978688308a8b6f8f9d
Ritesh Jaiswal <jaiswalritesh150@gmail.com>
2026-05-27 12:10:30 +0000

    container config done

--- 01.basic/03_conversionoperation.js
+++ 01.basic/03_conversionoperation.js
@@ -27,3 +27,25 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
 
+
+// ************** OPERATION ***************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "ritesh";
+let str2 = " jais";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log("1"+2+3);
