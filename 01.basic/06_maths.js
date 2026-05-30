5ab49a859c9a88d35d6c896bceeeaf9d3c902ab2
Ritesh Jaiswal <jaiswalritesh150@gmail.com>
2026-05-30 10:12:21 +0000

    Maths

---
+++ 01.basic/06_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(56.98));
+// console.log(Math.ceil(23.3));
+// console.log(Math.floor(65.9));
+// console.log(Math.min(4,9,2,4,0));
+// console.log(Math.max(51,9,80,2,76));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)))
